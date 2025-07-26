9ef2e9833cc544fd9cd5d6c4179e8e60fc885e83
Nico235711 <139637937+Nico235711@users.noreply.github.com>
2025-07-25 22:51:13 -0300

    the 3 challenges have been solved

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,5 +1,13 @@
 const fibonacci = (n) => {
-  // your code here
+  if (n === 0) return []
+  if (n === 1) return [1]
+  if (n === 2) return [1,1]
+  const fibo = [1,1]
+  for (let index = 2; index < n; index++) {
+    const sum = fibo[index - 1] + fibo[index - 2]
+    fibo.push(sum)
+  }
+  return fibo
 }
 
 module.exports = fibonacci;
--- src/primalidad.js
+++ src/primalidad.js
@@ -1,5 +1,9 @@
 const trialDivision = (number) => {
-  // your code here
+  let dividersAmount = 0
+  for (let index = 0; index <= number; index++) {
+    if (number % index === 0) dividersAmount++
+  }
+  return (dividersAmount === 2) ? true : false
 }
 
 module.exports = trialDivision;
